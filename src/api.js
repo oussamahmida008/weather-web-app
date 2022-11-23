548c98e4381b3291baa049112eb4c9a0e758420d
oussamahmida008 <103343616+oussamahmida008@users.noreply.github.com>
2022-11-23 20:43:38 +0100

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions  = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5cf121ba5amsh3ac55ca66f85069p19526fjsn09535007ea4d',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "291e752f16fc9c4fcd7a85e62fa00310";
+
